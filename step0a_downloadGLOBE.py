cc6ec16cce21674eefb9639f4136edb67a230f99
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-07-21 11:58:16 +0100

    added HTTP headers

--- step0a_downloadGLOBE.py
+++ step0a_downloadGLOBE.py
@@ -24,6 +24,12 @@ if not os.path.exists("all10g.zip"):
     # Start session ...
     sess = requests.Session()
     sess.allow_redirects = True
+    sess.headers.update({"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"})
+    sess.headers.update({"Accept-Language" : "en-GB,en;q=0.5"})
+    sess.headers.update({"Accept-Encoding" : "gzip, deflate"})
+    sess.headers.update({"DNT" : "1"})
+    sess.headers.update({"Upgrade-Insecure-Requests" : "1"})
+    sess.headers.update({"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"})
     sess.max_redirects = 5
 
     # Download the ZIP file ...
