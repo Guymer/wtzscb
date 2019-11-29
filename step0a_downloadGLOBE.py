b0cc20eb053cf4517bb967495614b95dfd9da109
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-11-29 12:41:21 +0000

    now uses pyguymer3 function

--- step0a_downloadGLOBE.py
+++ step0a_downloadGLOBE.py
@@ -3,12 +3,6 @@
 # Import standard modules ...
 import os
 
-# Import special modules ...
-try:
-    import requests
-except:
-    raise Exception("run \"pip install --user requests\"")
-
 # Import my modules ...
 try:
     import pyguymer3
@@ -22,19 +16,11 @@ if not os.path.exists("all10g.zip"):
     print("Downloading \"all10g.zip\" ...")
 
     # Start session ...
-    sess = requests.Session()
-    sess.allow_redirects = True
-    sess.headers.update({"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"})
-    sess.headers.update({"Accept-Language" : "en-GB,en;q=0.5"})
-    sess.headers.update({"Accept-Encoding" : "gzip, deflate"})
-    sess.headers.update({"DNT" : "1"})
-    sess.headers.update({"Upgrade-Insecure-Requests" : "1"})
-    sess.headers.update({"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"})
-    sess.max_redirects = 5
+    sess = pyguymer3.start_session()
 
     # Download the ZIP file ...
     if not pyguymer3.download_file(sess, "https://www.ngdc.noaa.gov/mgg/topo/DATATILES/elev/all10g.zip", "all10g.zip"):
         raise Exception("download failed", "https://www.ngdc.noaa.gov/mgg/topo/DATATILES/elev/all10g.zip")
 
-    # End session ...
+    # Close session ...
     sess.close()
