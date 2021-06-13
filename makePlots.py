b8695e8baf3b5dea092339087f44f50e67058ce3
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-06-13 13:12:00 +0100

    bugfix: actually import the new sub-module

--- makePlots.py
+++ makePlots.py
@@ -23,6 +23,7 @@ except:
 # Import my modules ...
 try:
     import pyguymer3
+    import pyguymer3.geo
 except:
     raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
 
